195de3c3a918bb69a5fea16121876e6ab9b8be73
treztreiz <mathias@wuaro.com>
2022-09-13 18:59:58 +0200

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>An event site for a better world</h2>
   </div>
 </template>
---
+++ src/views/EventList.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard'
+import EventService from '@/services/EventService'
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then((res) => {
+        this.events = res.data
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  },
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
